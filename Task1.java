e1ca7c0615ab10babd6571e945a86cdb5229579d
trisshogbac <trisshamae.ogbac20@gmail.com>
2026-07-16 06:18:57 +0000

    Task/Task1 - kamusta mundo submission

--- Task1.java
+++ Task1.java
@@ -1,5 +1,6 @@
 public class Task1 {
-  public static void main(String[] args) {
-    System.out.println ("Kamusta Mundo!");
-  }
+   public static void main (String [] args) {
+     System.out.println ("Kumusta Mundo!");
 }
+   }
+   
