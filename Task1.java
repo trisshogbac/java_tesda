bb20f3033eb6a9f72920c0166edb8e55b67f3292
trisshogbac <trisshamae.ogbac20@gmail.com>
2026-07-23 05:14:40 +0000

    Task2 submission - String concatenation

--- Task1.java
+++ Task1.java
@@ -1,5 +1,3 @@
-package Tasks;
-
 public class Task1 {
    public static void main (String [] args) {
      System.out.println ("Kumusta Mundo!");
---
+++ Task2.java
@@ -0,0 +1,17 @@
+public class Task2 {
+   //H3110 w0rld 2.0 true
+   //byte, short, int, float, boolean, char
+  public static void main(String[] args) {
+
+        byte b = 72; //converts H from ASCII
+        short three = 3; 
+        int one = 1;
+        float f = 2.0f;
+        boolean bol = true;
+        char c = 'd';
+
+        String word = "0 w0r";
+
+        System.out.println((char)b + "" + three + one + one + word + one + c + " " + f + " " + bol);
+    }
+}
